14bb8696358cd8c2546631de622317be2754c47b
Gabriel-Maran <gabriel.enzo.libero.maran@gmail.com>
2024-08-21 20:47:22 -0300

    Criação do projeto java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Integer idade = 10;
+        String nome = "Gabriel";
+        if(idade > 21){
+            System.out.println("É maior que 21 anos");
+        } else{
+            System.out.println("Não é maior que 21");
+        }
+
+        /*
+        equal compara Textos
+         */
     }
 }
